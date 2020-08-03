131a23056f43a005ca1704f9dc7578be50ec8bf6
xiaxueqiu <895034798@qq.com>
2020-08-03 18:12:26 +0800

    Merge branch 'dev1'
    # Conflicts:
    #	gitTest/src/FirstClass.java

--- gitTest/src/FirstClass.java
+++ gitTest/src/FirstClass.java
@@ -1,17 +1,9 @@
+import java.util.HashMap;
+
 public class FirstClass {
     public static void main(String[] args) {
-        ;  //我是最哈珀的
-        ;
-        //我是最大的一个领袖
-        System.out.println("wdddd");
-        //我自己来提交
-        //我是最棒的
-        //我自己来提交
-        //我自己来333333
-      //  dddd
-        ;
-        //我是最大的一个领袖
-        ;
-        //我是no 1
+        HashMap<String,String> map=new HashMap<>();
+        map.put("1","2");
+        System.out.println(map.get("1"));
     }
 }
